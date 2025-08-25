d0aed52d163fafdff55e490adf79c3cfeaf65b3b
Marcus Hagen <marcuh17@uia.no>
2025-08-25 21:35:25 +0200

    TaskV slimmed down

--- assignment_1/taskV.py
+++ assignment_1/taskV.py
@@ -1,30 +1,14 @@
 import cv2
 
-# Open the default camera
 cam = cv2.VideoCapture(0)
 
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cam.get(cv2.CAP_PROP_FPS)
+width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+with open("assignment_1/solutions/camera_outputs.txt", "w") as f:
+    f.write(f"fps: {fps}\n")
+    f.write(f"height: {height}\n")
+    f.write(f"width: {width}\n")
 
-while True:
-    ret, frame = cam.read()
-
-    # Write the frame to the output file
-    out.write(frame)
-
-    # Display the captured frame
-    cv2.imshow('Camera', frame)
-
-    # Press 'q' to exit the loop
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Release the capture and writer objects
 cam.release()
-out.release()
-cv2.destroyAllWindows()
